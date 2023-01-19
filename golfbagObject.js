f48da1fe8ca8236fa91f24c9559643c3f145c297
Korny94 <karlmagnusnokling@hotmail.no>
2023-01-19 19:10:08 +0100

    update

--- golfbagObject.js
+++ golfbagObject.js
@@ -14,10 +14,64 @@ let golfbag = {
 
 
 
-        // Nested Objects
+        // Nested & Array of Objects
 
 let golfbag = {
-    golfclubs: ["Driver", "Wood", "Hybrid", "4-iron", "5-iron", "6-iron", "7-iron", "8-iron", "9-iron", "pitching-wedge", "gap-wedge", "sand-wedge", "lob-wedge", "putter"],
+    golfclubs: [{
+        club: "Driver",
+        loftDegrees: 9.0
+    },
+    {
+        club: "3-Wood",
+        loftDegrees: 15.0
+    },
+    {
+        club: "3-hybrid",
+        loftDegrees: 19.0
+    },
+    {
+        club: "4-iron",
+        loftDegrees: 22.5
+    },
+    {
+        club: "5-iron",
+        loftDegrees: 25.5
+    },
+    {
+        club: "6-iron",
+        loftDegrees: 29.0
+    },
+    {
+        club: "7-iron",
+        loftDegrees: 33.0
+    },
+    {
+        club: "8-iron",
+        loftDegrees: 37.0
+    },
+    {
+        club: "9-iron",
+        loftDegrees: 41.5
+    },
+    {
+        club: "pitching-wedge",
+        loftDegrees: 46.0
+    },{
+        club: "gap-wedge",
+        loftDegrees: 51.0
+    },
+    {
+        club: "sand-wedge",
+        loftDegrees: 54.0
+    },
+    {
+        club: "lob-wedge",
+        loftDegrees: 58.0
+    },
+    {
+        club: "putter",
+        loftDegrees: 3.5
+    }],
     model: {
         bag: "Cobra Ultralight Cart Bag",
         driver: "Callaway Epic GBB Sub Zero",
@@ -41,6 +95,15 @@ let golfbag = {
         let totalPrice = golfbag.price.bag + golfbag.price.driver + golfbag.price.wood + golfbag.price.hybrid + golfbag.price.irons + golfbag.price.wedges + golfbag.price.putter;
     }
 }
+
+const imgGolfBag = document.getElementById("golfbag");
+
+let bagModel = document.getElementById("bagModel");
+
+imgGolfBag.onmouseover = function() {
+        bagModel.style.opacity = 1;
+}
+
 
 const listDriver = document.getElementById("listDriver");
 
@@ -68,7 +131,7 @@ let imgPutter = document.getElementById("putter");
 
 let imgDefault = document.getElementById("defaultClub");
 
-let clubModel = document.querySelector("h3");
+let clubModel = document.querySelector("#clubModel");
 
 
 listDriver.onmouseover = function() {
